5c96c126c6997d7a7a0d448560ff63e6abe345ca
YGRL <ryan47@kimo.com>
2019-10-07 11:43:05 -0300

    ajuste

--- app/src/main/java/com/example/senac/ferramentas20/AcelerometroFragment.java
+++ app/src/main/java/com/example/senac/ferramentas20/AcelerometroFragment.java
@@ -1,16 +1,13 @@
 package com.example.senac.ferramentas20;
 
-import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.os.Vibrator;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,8 +15,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 import static android.content.Context.SENSOR_SERVICE;
-import static androidx.core.content.ContextCompat.getSystemService;
 
 public class AcelerometroFragment extends Fragment implements SensorEventListener {
     //import sensor
@@ -31,31 +28,28 @@ public class AcelerometroFragment extends Fragment implements SensorEventListene
     float[] history = new float[2];
     String[] direcao = {"NONE", "NONE"};
 
-    ImageView diresao,diresao2;
+    ImageView diresao;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_acelerometro, container, false);
         //Find id
         direction = view.findViewById(R.id.direction);
         diresao = view.findViewById(R.id.diresao);
-        diresao2 = view.findViewById(R.id.diresao2);
 
         //Get Sensor
         sensorManager = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, accelerometer, 1000000);
-//        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
         return view;
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
 //        Log.e("teste", "X: " + sensorEvent.values[0] + " Y: " + sensorEvent.values[1] + " Z: " + sensorEvent.values[2]);
-
         float xChange = history[0] - sensorEvent.values[0];
         float yChange = history[1] - sensorEvent.values[1];
         history[0] = sensorEvent.values[0]; //sensorEvent.values[0] x
@@ -66,18 +60,16 @@ public class AcelerometroFragment extends Fragment implements SensorEventListene
         if (xChange > 2) {
             direcao[0] = "RIGHT";
             diresao.setImageResource(R.drawable.setaright);
-
         } else if (xChange < -2) {
             direcao[0] = "LEFT";
             diresao.setImageResource(R.drawable.setaleft);
         }
-
         if (yChange > 2) {
             direcao[1] = "DOWN";
-            diresao2.setImageResource(R.drawable.setadown);
+            diresao.setImageResource(R.drawable.setadown);
         } else if (yChange < -2) {
             direcao[1] = "UP";
-            diresao2.setImageResource(R.drawable.setaup);
+            diresao.setImageResource(R.drawable.setaup);
         }
 
         builder.setLength(0);
@@ -85,10 +77,8 @@ public class AcelerometroFragment extends Fragment implements SensorEventListene
         builder.append(direcao[0]);
         builder.append(" Y: ");
         builder.append(direcao[1]);
-
         direction.setText(builder.toString());
         Log.e("teste", builder.toString());
-
     }
 
     @Override
