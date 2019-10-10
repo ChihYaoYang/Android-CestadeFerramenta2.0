26d067dbba5747bcb8fd5c2b8ae49423f96db2ee
YGRL <ryan47@kimo.com>
2019-10-10 18:09:07 -0300

    add QR_Code

--- app/src/main/java/com/example/senac/ferramentas20/QRcodeFragment.java
+++ app/src/main/java/com/example/senac/ferramentas20/QRcodeFragment.java
@@ -7,38 +7,97 @@ import android.os.Bundle;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 
 public class QRcodeFragment extends Fragment {
-    Button qr;
+    //Declara variável
+    SurfaceView surfaceView;
+    TextView textView;
+    //Declara câmera
+    CameraSource cameraSource;
+    //Declara plugins do Google (Vision)(QR_Code)
+    BarcodeDetector barcodeDetector;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_qr_code, container, false);
-        //find ID
-        qr = view.findViewById(R.id.qr);
 
-        //Onclick
-        qr.setOnClickListener(new View.OnClickListener() {
+        //check permissão
+        permissao_camera();
+
+        //find ID
+        surfaceView = view.findViewById(R.id.surfaceView);
+        textView = view.findViewById(R.id.qrtextview);
+
+        barcodeDetector = new BarcodeDetector.Builder(getActivity()).setBarcodeFormats(Barcode.QR_CODE).build();
+        cameraSource = new CameraSource.Builder(getActivity(), barcodeDetector).setRequestedPreviewSize(300, 300).build();
+        //讓SurfaceView上面顯示東西
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            //把相機畫面印在上面
             @Override
-            public void onClick(View view) {
-                scannerQR();
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                try {
+                    cameraSource.start(surfaceHolder);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            //在關閉的同時就會關掉相機
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                //讓他判斷是不是有掃描到條碼，有的話就顯示出來
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+                if (qrCodes.size() != 0) {
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(qrCodes.valueAt(0).displayValue);
+                        }
+                    });
+                }
             }
         });
         return view;
     }
 
 
-    public void scannerQR() {
+    public void permissao_camera() {
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, 1);
-        } else {
-
         }
     }
 }
